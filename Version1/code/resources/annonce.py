52d0f0e1ccaac6cbbb51c35014b36e4ba4eedb01
Samih Abbassi <Venus@MacBook-Pro-de-Samih.local>
2017-06-06 17:49:45 +0200

    deposer annonce en récupérant id user

--- Version1/code/resources/annonce.py
+++ Version1/code/resources/annonce.py
@@ -1,34 +1,40 @@
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
+from flask_jwt import jwt_required, current_identity
 from models.annonce import AnnonceModel
 
 class Annonce(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('theme',
-        type=float,
+        type=str,
         required=True,
         help="This field cannot be left blank!"
     )
-    parser.add_argument('user_id',
-        type=int,
+    parser.add_argument('description',
+        type=str,
         required=True,
-        help="Every item needs a user_id."
+        help="This field cannot be left blank!"
     )
+    # parser.add_argument('user_id',
+    #     type=int,
+    #     required=True,
+    #     help="Every item needs a user_id."
+    # )
 
     @jwt_required()
     def get(self, name):
         annonce = AnnonceModel.find_by_name(name)
         if annonce:
             return annonce.json()
-        return {'message': 'Item not found'}, 404
+        return {'message': 'Annonce not found'}, 404
 
+    @jwt_required()
     def post(self, name):
         if AnnonceModel.find_by_name(name):
             return {'message': "An annonce with name '{}' already exists.".format(name)}, 400
 
         data = Annonce.parser.parse_args()
 
-        annonce = AnnonceModel(name, data['theme'], data['description'], data['user_id'])
+        annonce = AnnonceModel(name, data['theme'], data['description'], current_identity.id)
 
         try:
             annonce.save_to_db()
@@ -50,7 +56,7 @@ class Annonce(Resource):
         annonce = AnnonceModel.find_by_name(name)
 
         if annonce:
-            annonce.price = data['theme']
+            annonce.theme = data['theme']
         else:
             annonce = AnnonceModel(name, data['annonce'])
 
